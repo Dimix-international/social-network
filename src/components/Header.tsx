5f0ca85c5793282e9f32c751f149c082e37bafcf
Dima Melnikov <dima.dimix94@mail.ru>
2021-07-23 16:22:15 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Header = () => {
+    return(
+        <header className="header">
+            <a href="#" className="header__logo">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Apple_Computer_Logo_rainbow.svg/1200px-Apple_Computer_Logo_rainbow.svg.png"
+                    alt=""/>
+            </a>
+        </header>
+    )
+}
