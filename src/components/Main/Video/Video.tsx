d67ed958a57670227d93cd507cda31f6144abe89
Dimix-international <dima.dimix94@mail.ru>
2021-08-21 21:40:37 +0300

    come back after testing

--- src/components/Main/Video/Video.tsx
+++ src/components/Main/Video/Video.tsx
@@ -3,18 +3,8 @@ import c from './Video.module.scss'
 
 export const Video = () => {
     return (
-        <div className={c.music}>
-            <h1>Video</h1>
+        <div className={c.video}>
             Video
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, qui?</p>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
-            <hr/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, quaerat!</p>
         </div>
     )
 }
