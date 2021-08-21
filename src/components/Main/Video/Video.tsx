022243bbb5a17f8559ecf27a96f171efe9753a4d
Dimix-international <dima.dimix94@mail.ru>
2021-08-21 21:17:26 +0300

    change video component

--- src/components/Main/Video/Video.tsx
+++ src/components/Main/Video/Video.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import c from './Video.module.scss'
 
 export const Video = () => {
+    function add(a:number,b:number) {
+        return a + b;
+    }
     return (
         <div className={c.music}>
             Video
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, qui?</p>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+            <hr/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, quaerat!</p>
         </div>
     )
 }
